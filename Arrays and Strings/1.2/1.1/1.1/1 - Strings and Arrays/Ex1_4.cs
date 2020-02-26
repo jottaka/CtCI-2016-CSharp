81cb5c19120bf39f7341a867a594680dbc0f588d
francisco <fjlimal@gmail.com>
2020-02-26 10:29:33 -0300

    Optimized 1.4 code

--- Arrays and Strings/1.2/1.1/1.1/1 - Strings and Arrays/Ex1_4.cs
+++ Arrays and Strings/1.2/1.1/1.1/1 - Strings and Arrays/Ex1_4.cs
@@ -1,8 +1,11 @@
 ﻿/*
 CiCI - 2015
 1 - Arrays and strings
-1.2 - Check Permutation: Given two strings, write a method to decide if one is a permutation of the
+1.4 -  Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. 
 other. 
+
+
 
 */
 
@@ -27,7 +30,7 @@ namespace CtDI
                 var inStr = data[0];
                 var value = bool.Parse(data[1]);
 
-                if (hasPalindromeNaive(inStr) == value)
+                if (hasPalindromesOptimized(inStr) == value)
                 {
                     HelperCtCI.AddSuccess();
                 }
@@ -45,13 +48,53 @@ namespace CtDI
             throw new NotImplementedException();
         }
 
+
+
+        private bool hasPalindromesOptimized(string str)
+        {
+            var hasPalindrome = false;
+            var processedString = str.ToLower().Replace(" ", "");
+
+            // a palindrome must have N even count of equal chars and at maximun a odd count 
+            var groupedCharCount = processedString.GroupBy(c => c)
+                         .Select(c => new
+                         {
+                             Char = c.Key,
+                             Count = c.Count()
+                         });
+            var sum = groupedCharCount.Sum(v => (v.Count % 2));
+            if (sum > 1) { return false; }
+
+            var stringToPermuteList = groupedCharCount.ToList().ConvertAll(
+                s => new String(new char[s.Count / 2]).Replace('\0', s.Char)
+                );
+            var uniqueChar = groupedCharCount.FirstOrDefault(c => c.Count == 1)?.Char;
+            var stringToPermute = string.Concat(stringToPermuteList);
+            foreach (var halfPermutation in getPermutationsOptimized(stringToPermute))
+            {
+                var inverseHalfPermutation = string.Concat(halfPermutation.ToArray().Reverse());
+                //var p1 = string.Concat(halfPermutation, $"{uniqueChar}", inverseHalfPermutation);
+                var p2 = string.Concat(inverseHalfPermutation, $"{uniqueChar}", halfPermutation);
+
+
+
+                //Console.WriteLine($"Palindrome:{p1}");
+
+                Console.WriteLine($"Palindrome:{p2}");
+                hasPalindrome = true;
+            }
+            return hasPalindrome;
+        }
+
+
         private bool hasPalindromeNaive(string str1)
         {
             var hasPalindrome = false;
             var processedString = str1.ToLower().Replace(" ", "");
+
             if (isPalindrome(processedString)) { return true; }
-            
-            foreach( var permutation in getPermutationsNaive(processedString))
+
+            foreach (var permutation in getPermutationsNaive(processedString))
             {
                 //Console.WriteLine(permutation);
                 if (isPalindrome(permutation))
@@ -75,8 +118,7 @@ namespace CtDI
             var currentChar = input.ElementAt(0);
             var subStr = input.Remove(0, 1);
             var list = getPermutationsNaive(subStr);
-            var toReturnList = new List<string>();
-            foreach (var permutation in list )
+            foreach (var permutation in list)
             {
                 for (var i = 0; i < input.Length; i++)
                 {
@@ -86,6 +128,45 @@ namespace CtDI
 
             return repetitionSet.ToList();
         }
+
+        private IEnumerable<string> getPermutationsOptimized(string input)
+        {
+            var repetitionSet = new HashSet<string>();
+            var charSet = new HashSet<char>();
+            if (input.Length <= 1)
+            {
+                yield return input;
+            }
+            else
+            {
+                var distinctChars = new string(input.Distinct().ToArray());
+                var halfIdx = (int)Math.Ceiling(distinctChars.Length / 2.0);
+                distinctChars = distinctChars.Remove(halfIdx, distinctChars.Length-halfIdx) ;
+                foreach (var dc in distinctChars)
+                {
+                    var subStr = input.Remove(input.IndexOf(dc),1);
+                    foreach (var permutation in getPermutationsOptimized(subStr))
+                    {
+                        var perm1 = permutation.Insert(0, $"{dc}");
+                        var perm2 = string.Concat(perm1.ToArray().Reverse());
+                        if (!repetitionSet.Contains(perm1))
+                        {
+                            repetitionSet.Add(perm1);
+                            yield return perm1;
+                        }
+                        if (!repetitionSet.Contains(perm2))
+                        {
+                            repetitionSet.Add(perm2);
+                            yield return perm2;
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+
 
         private bool isPalindrome(string input)
         {
